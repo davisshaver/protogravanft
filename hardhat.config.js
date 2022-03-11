802e5aee2658888711e330f7cb7c2a642ce77bd8
Davis Shaver <davisshaver@gmail.com>
2022-03-11 16:33:45 -0500

    :construction: Start adding some Hardhat support

---
+++ hardhat.config.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.12",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      },
+    },
+  },
+};
